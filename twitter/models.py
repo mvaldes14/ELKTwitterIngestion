e830584f490412a14a980bace462184a89c71c69
rorixrebel <elxilote@gmail.com>
2018-12-27 15:27:56 -0600

    fixed timezone for timestamp, ingestion working

--- twitter/models.py
+++ twitter/models.py
@@ -1,19 +1,58 @@
 from textblob import TextBlob
 from elasticsearch import Elasticsearch
+import uuid
+import json
+from datetime import datetime
 
+# Elastic Connection
+es = Elasticsearch(hosts="localhost")
+index_name = 'trump-' + datetime.now().strftime('%Y.%m.%d')
 
+def check_elastic():
+    if not es.indices.exists('trump'):
+        mapping = """
+        {
+        "settings": {
+            "index.number_of_shards": 1,
+            "index.number_of_replicas": 0
+        }
+        }
+        """
+        es.indices.create(index=index_name, body=mapping)
+
+   
+
+# Data Model
 class Tweet(object):
-    def __init__(self, username, tweet_text, hashtags):
+    def __init__(self, username, realname, location, tweet_text, hashtags):
+        self.id = str(uuid.uuid4())
+        self.timestamp = datetime.utcnow()
         self.username = username
+        self.realname = realname
+        self.location = location
         self.tweet_text = tweet_text
-        self.hashtags = [hash for hash in hashtags if hash]
+        self.hashtags = [hash["text"] for hash in hashtags]
         self.sentiment = self.get_sentiment()
 
     def get_sentiment(self):
         return TextBlob(self.tweet_text).sentiment.polarity
 
+    def push_to_elastic(self):
+        es.index(
+            index=index_name,
+            doc_type="tweets",
+            id=self.id,
+            body={
+                    "@timestamp": self.timestamp,
+                    "user": self.username,
+                    "realname": self.realname,
+                    "location": self.location,
+                    "tweet": self.tweet_text,
+                    "hashtags": self.hashtags,
+                    "sentiment": self.sentiment,
+                }
+        )
+    
     def get_details(self):
-        return (self.username, self.tweet_text, self.hashtags, self.sentiment)
+        print(self.timestamp, self.username, self.tweet_text, self.hashtags, self.sentiment)
 
-#es = Elasticsearch(hosts='localhost')
-#print(es.cat.health())
--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,6 +1,6 @@
-from twython import Twython
 from twython import TwythonStreamer
-from models import Tweet
+from models import Tweet, es
+from datetime import datetime
 
 CONSUMER_KEY = "V1XCyXScnXDZh232FAIvjSd2e"
 CONSUMER_SECRET = "JOB36LqZhd8A6WJ4zmd7ZBy3As2CQhK9uLXonlpaliFJYMhcSv"
@@ -8,20 +8,20 @@ AUTH_TOKEN = "43233942-pMGIuekO53ZywjjOe3JVZJoekOLWN5KzIfj1NcvKP"
 AUTH_SECRET = "ibXRDGgO4g9WA8buMFvzfyJebA6JkhCN26WMUeGeA"
 
 
-# twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, AUTH_TOKEN, AUTH_SECRET)
-# tweets = twitter.search(q="Trump", count=1, tweet_mode="extended")
-# for i in tweets["statuses"]:
-#     print(i)
-#     tweet1 = Tweet(
-#         username=i["user"]["screen_name"],
-#         tweet_text=i["full_text"],
-#         hashtags=i["entities"]["hashtags"],
-#     )
-#     print(tweet1.get_details())
-
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
-            print(data)
+        try:
+            tweets = Tweet(
+                username=data["user"]["screen_name"],
+                realname=data["user"]["name"],
+                location=data["user"]["location"],            
+                tweet_text=data["text"],
+                hashtags=data["entities"]["hashtags"],
+            )
+            tweets.push_to_elastic()
+        except KeyError:
+            pass        
+        
 
     def on_error(self, status_code, data):
         print(status_code)
@@ -32,7 +32,10 @@ class MyStreamer(TwythonStreamer):
         print("Request timed out, try again later")
         self.disconnect()
 
-stream = MyStreamer(CONSUMER_KEY, CONSUMER_SECRET, AUTH_TOKEN, AUTH_SECRET)
-stream.statuses.filter(track='python')
+def start():
+    stream = MyStreamer(CONSUMER_KEY, CONSUMER_SECRET, AUTH_TOKEN, AUTH_SECRET)
+    stream.statuses.filter(track=["Trump", "trump"])
 
 
+if __name__ == "__main__":
+    start()
